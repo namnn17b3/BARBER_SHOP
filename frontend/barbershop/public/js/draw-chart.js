aa06c90d529356aca97246941d4125f3fc9a8aa4
namnn-1331 <nguyen.ngoc.nam@sun-asterisk.com>
2024-11-19 15:03:02 +0700

    add option bar chart

--- frontend/barbershop/public/js/draw-chart.js
+++ frontend/barbershop/public/js/draw-chart.js
@@ -2,25 +2,26 @@ let chart = null;
 
 const drawChart = () => {
   const chartElement = document.getElementById('inline-chart');
-  // chartElement.innerHTML = '';
-  // chartElement.setAttribute('style', '');
   if (chart) {
     chart.destroy();
   }
   const dataFromJson = JSON.parse(document.querySelector('#api-data-chart').innerText);
   const chartType = [...document.querySelectorAll(`input[name="chart-stats-dropdown-chart"`)].find((item) => item.checked);
+  let fnDrawChart = null;
   if (chartType.value === 'line') {
-    drawLineChart(
+    fnDrawChart = drawLineChart;
+  } else if (chartType.value === 'circle') {
+    fnDrawChart = drawCircleChart;
+  } else if (chartType.value === 'bar') {
+    fnDrawChart = drawBarChart;
+  }
+
+  if (fnDrawChart) {
+    fnDrawChart(
       chartElement,
       dataFromJson.map(item => item.totalAmount),
       dataFromJson.map(item => item.date ? item.date : item.month),
     );
-  } else if (chartType.value === 'circle') {
-    drawCircleChart(
-      chartElement,
-      dataFromJson.map(item => item.totalAmount),
-      dataFromJson.map(item => item.date ? item.date : item.month),
-    )
   }
 }
 
@@ -101,62 +102,6 @@ function drawCircleChart(chartElement, data, labels) {
     },
   }
 
-  // const options = {
-  //   series: [52.8, 26.8, 20.4],
-  //   colors: ["#1C64F2", "#16BDCA", "#9061F9"],
-  //   chart: {
-  //     height: 420,
-  //     width: "100%",
-  //     type: "pie",
-  //   },
-  //   stroke: {
-  //     colors: ["white"],
-  //     lineCap: "",
-  //   },
-  //   plotOptions: {
-  //     pie: {
-  //       labels: {
-  //         show: true,
-  //       },
-  //       size: "100%",
-  //       dataLabels: {
-  //         offset: -25
-  //       }
-  //     },
-  //   },
-  //   labels: ["Direct", "Organic search", "Referrals"],
-  //   dataLabels: {
-  //     enabled: true,
-  //     style: {
-  //       fontFamily: "Inter, sans-serif",
-  //     },
-  //   },
-  //   legend: {
-  //     position: "bottom",
-  //     fontFamily: "Inter, sans-serif",
-  //   },
-  //   yaxis: {
-  //     labels: {
-  //       formatter: function (value) {
-  //         return value + "%"
-  //       },
-  //     },
-  //   },
-  //   xaxis: {
-  //     labels: {
-  //       formatter: function (value) {
-  //         return value  + "%"
-  //       },
-  //     },
-  //     axisTicks: {
-  //       show: false,
-  //     },
-  //     axisBorder: {
-  //       show: false,
-  //     },
-  //   },
-  // }
-
   if (chartElement && typeof ApexCharts !== 'undefined') {
     chart = new ApexCharts(chartElement, options);
     chart.render();
@@ -228,6 +173,102 @@ function drawLineChart(chartElement, data, labels) {
     },
     yaxis: {
       show: false,
+    },
+  }
+
+  if (chartElement && typeof ApexCharts !== 'undefined') {
+    chart = new ApexCharts(chartElement, options);
+    chart.render();
+  }
+}
+
+function drawBarChart(chartElement, data, labels) {
+  const options = {
+    // colors: ["#1A56DB", "#FDBA8C"],
+    series: [
+      {
+        name: "Revenue",
+        color: generateRandomColors(1)[0],
+        data: data.map((item, idx) => {
+          return {
+            x: labels[idx],
+            y: item,
+          }
+        }),
+      },
+    ],
+    chart: {
+      type: "bar",
+      height: "320px",
+      fontFamily: "Inter, sans-serif",
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "70%",
+        borderRadiusApplication: "end",
+        borderRadius: 8,
+      },
+    },
+    tooltip: {
+      shared: true,
+      intersect: false,
+      style: {
+        fontFamily: "Inter, sans-serif",
+      },
+    },
+    states: {
+      hover: {
+        filter: {
+          type: "darken",
+          value: 1,
+        },
+      },
+    },
+    stroke: {
+      show: true,
+      width: 0,
+      colors: ["transparent"],
+    },
+    grid: {
+      show: false,
+      strokeDashArray: 4,
+      padding: {
+        left: 2,
+        right: 2,
+        top: -14
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    legend: {
+      show: false,
+    },
+    xaxis: {
+      floating: false,
+      labels: {
+        show: true,
+        style: {
+          fontFamily: "Inter, sans-serif",
+          cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+        }
+      },
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+    },
+    yaxis: {
+      show: false,
+    },
+    fill: {
+      opacity: 1,
     },
   }
 
